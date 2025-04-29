499f9162a871a46a15ff663318ac8a425b873d77
Sreevathsava <sreevathsava.pak@tigeranalytics.com>
2025-04-29 13:11:56 +0530

    trained new model

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,6 +1,6 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-
+from sklearn.preprocessing import StandardScaler
 from src.models.classifier import SklearnClassifier
 from src.utils.config import load_config
 from src.utils.guardrails import validate_evaluation_metrics
@@ -14,11 +14,17 @@ def main():
 
     df = store.get_processed("transformed_dataset.csv")
     df_train, df_test = train_test_split(df, test_size=config["test_size"])
-
+    scaler= StandardScaler()
+    df_train.loc[:, config["features"]] = scaler.fit_transform(df_train[config["features"]])
+    df_test.loc[:, config["features"]] = scaler.transform(df_test[config["features"]])
+    df_train.loc[:, config["target"]] = df_train[config["target"]].astype(int)
+    df_test.loc[:, config["target"]] = df_test[config["target"]].astype(int)
+    
     rf_estimator = RandomForestClassifier(**config["random_forest"])
     model = SklearnClassifier(rf_estimator, config["features"], config["target"])
     model.train(df_train)
-
+    feature_importances = model.clf.feature_importances_
+    print(f"Feature Importances: {feature_importances}")
     metrics = model.evaluate(df_test)
 
     store.put_model("saved_model.pkl", model)
