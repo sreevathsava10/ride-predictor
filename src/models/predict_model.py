0a91ab5686866ceba085cb71721ae26bea5eab90
Sreevathsava <sreevathsava.pak@tigeranalytics.com>
2025-04-29 13:16:14 +0530

    modified predict_model.py

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pandas as pd
-
 from src.features.build_features import apply_feature_engineering
 from src.utils.guardrails import validate_prediction_results
 from src.utils.store import AssignmentStore
@@ -9,10 +8,12 @@ from src.utils.store import AssignmentStore
 @validate_prediction_results
 def main():
     store = AssignmentStore()
-
+    df_train = store.get_processed("transformed_dataset.csv")
     df_test = store.get_raw("test_data.csv")
-    df_test = apply_feature_engineering(df_test)
-
+    customer_model=store.get_model("pickup_cluster.pkl")
+    driver_model=store.get_model("driver_cluster.pkl")
+    df_test = apply_feature_engineering(df_test,historical_data=df_train,c_cluster=customer_model,d_cluster=driver_model)
+    store.put_processed("transformed_test_data.csv", df_test)
     model = store.get_model("saved_model.pkl")
     df_test["score"] = model.predict(df_test)
 
